081f2cefd71c6fba0f829a6f7e015a96aad4ed37
gj026621 <116082443+gj026621@users.noreply.github.com>
2024-11-23 21:45:42 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "En vrac",
-  "body": " En vrac   Facteurs de conversion  Multiplier une quantité par 1 ne la modifie pas. Cette astuce permet de faire apparaître l'unité souhaitée lors d'une conversion.     Convertissez 12 gigamètres en micromètres. Utilisez la notation scientifique pour exprimer votre réponse.                          Combien y a-t-il de pieds dans verges? Utilisez la notation décimale pour exprimer votre réponse.          pieds               Combien y a-t-il de micromètres dans milles? Utilisez la notation scientifique pour exprimer votre réponse. Arrondissez la mantisse à deux décimales.                          Un chat est un mammifère à quatre pattes.     Une table    Variable  Variable  Conjonction  Disjonction    V  V  V  V    V  F  F  V    F  V  F  V    F  F  F  F       Un tiens vaut mieux que deux tu l'auras.              La fonction sinus avec TikZ en latex        Levez les bras et faites un tour sur vous-même. Quel âge la Terre a-t-elle?    milliards d'années environ    Forcez-vous à utiliser TikZ.   Essayez plus fort.   Utilisez ChatGPT au besoin.    On veut déterminer la quantité de chaleur nécessaire pour porter l'eau d'une bouilloire à ébullition alors qu'elle est à  61   .  Pour cela, on doit connaître la capacité thermique de l'eau à cette température, mais on ne dispose que des données suivantes : où est en    et est en      .  Estimez la valeur de à  61   à l'aide du polynôme interpolateur du degré spécifié en choisissant vos points grâce aux données ci-dessus, de façon à ce que les valeurs de soient les plus proches possible de  61   tout en l'encadrant :  avec un polynôme interpolateur de degré un;  avec un polynôme interpolateur de degré deux.          On choisit d'interpoler les points suivants : La forme de Lagrange du polynôme interpolateur est alors et on a On estime ainsi que vaut environ  4191,16     à  61   .       "
+  "body": " En vrac   Facteurs de conversion  Multiplier une quantité par 1 ne la modifie pas. Cette astuce permet de faire apparaître l'unité souhaitée lors d'une conversion.     Convertissez 12 gigamètres en micromètres. Utilisez la notation scientifique pour exprimer votre réponse.                          Combien y a-t-il de pieds dans verges? Utilisez la notation décimale pour exprimer votre réponse.          pieds               Combien y a-t-il de micromètres dans milles? Utilisez la notation scientifique pour exprimer votre réponse. Arrondissez la mantisse à deux décimales.                          Un chat est un mammifère à quatre pattes qui peut miauler.     Un chat avec TikZ en latex       Une table    Variable  Variable  Conjonction  Disjonction    V  V  V  V    V  F  F  V    F  V  F  V    F  F  F  F       Un tiens vaut mieux que deux tu l'auras.              La fonction sinus avec TikZ en latex        Levez les bras et faites un tour sur vous-même. Quel âge la Terre a-t-elle?    milliards d'années environ    Forcez-vous à utiliser TikZ.   Essayez plus fort.   Utilisez ChatGPT au besoin.    On veut déterminer la quantité de chaleur nécessaire pour porter l'eau d'une bouilloire à ébullition alors qu'elle est à  61   .  Pour cela, on doit connaître la capacité thermique de l'eau à cette température, mais on ne dispose que des données suivantes : où est en    et est en      .  Estimez la valeur de à  61   à l'aide du polynôme interpolateur du degré spécifié en choisissant vos points grâce aux données ci-dessus, de façon à ce que les valeurs de soient les plus proches possible de  61   tout en l'encadrant :  avec un polynôme interpolateur de degré un;  avec un polynôme interpolateur de degré deux.          On choisit d'interpoler les points suivants : La forme de Lagrange du polynôme interpolateur est alors et on a On estime ainsi que vaut environ  4191,16     à  61   .       "
 },
 {
   "id": "ex-conversion-1",
@@ -52,14 +52,23 @@ var ptx_lunr_docs = [
   "type": "Définition",
   "number": "1.1.4",
   "title": "",
-  "body": "  Un chat est un mammifère à quatre pattes.   "
+  "body": "  Un chat est un mammifère à quatre pattes qui peut miauler.   "
 },
 {
-  "id": "sec-section-name-7",
+  "id": "fig-tikz-chat",
   "level": "2",
-  "url": "sec-section-name.html#sec-section-name-7",
-  "type": "Table",
+  "url": "sec-section-name.html#fig-tikz-chat",
+  "type": "Figure",
   "number": "1.1.5",
+  "title": "",
+  "body": " Un chat avec TikZ en latex     "
+},
+{
+  "id": "sec-section-name-8",
+  "level": "2",
+  "url": "sec-section-name.html#sec-section-name-8",
+  "type": "Table",
+  "number": "1.1.6",
   "title": "Une table",
   "body": " Une table    Variable  Variable  Conjonction  Disjonction    V  V  V  V    V  F  F  V    F  V  F  V    F  F  F  F    "
 },
@@ -68,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-name.html#thm-vrac",
   "type": "Théorème",
-  "number": "1.1.6",
+  "number": "1.1.7",
   "title": "",
   "body": "  Un tiens vaut mieux que deux tu l'auras.            "
 },
@@ -77,7 +86,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "sec-section-name.html#fig-tikz",
   "type": "Figure",
-  "number": "1.1.7",
+  "number": "1.1.8",
   "title": "",
   "body": " La fonction sinus avec TikZ en latex     "
 },
